433cb98d41b12bd815b929ecc04ed2bf2eb6e535
Josep Roure Alcobé <roure@tecnocampus.cat>
2019-09-19 16:45:35 +0200

    optimized imports

--- src/main/java/cat/tecnocampus/configuration/KnightConfiguration.java
+++ src/main/java/cat/tecnocampus/configuration/KnightConfiguration.java
@@ -1,14 +1,7 @@
 package cat.tecnocampus.configuration;
 
-import cat.tecnocampus.domain.BraveKnight;
-import cat.tecnocampus.domain.DamselRescuingKnight;
-import cat.tecnocampus.domain.Quest;
-import cat.tecnocampus.domain.SlayDragonQuest;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * Created by roure on 28/09/2016.
